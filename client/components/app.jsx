5bd259dce4516db9d5868520fc31b4e855113f69
Christian Haney <christianhaney1996@gmail.com>
2019-11-19 12:53:39 -0800

    Shopping Cart | Frontend - Christian Haney (functionality) created constructor and setView method in App component constructor method defines and ititial state with one property view setView method changes the App components view state based on parameters passed in modified render method of App component to pass the setView method as a callback down to the ProductList component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,12 +3,22 @@ import Header from './Header';
 import ProductList from './product-list';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { view: { name: 'catalog', params: {} } };
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(name, params) {
+    this.setState({ name, params });
+  }
+
   render() {
     return (
       <div className='app'>
         <Header title='Wicked Sales'/>
         <div className='container'>
-          <ProductList/>
+          <ProductList setView={this.setView}/>
         </div>
       </div>
     );
