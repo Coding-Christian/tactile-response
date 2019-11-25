c35c74ace350e9988576e04bdb06b7d97fc3a24d
Christian Haney <christianhaney1996@gmail.com>
2019-11-25 14:53:25 -0800

    Shopping Cart | Frontend - Christian Haney (functionality) modified render method of CheckoutForm to display a form for inputting customer name, address, and card number

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -8,10 +8,37 @@ class CheckoutForm extends React.Component {
       creditCard: '',
       address: ''
     };
+    this.setView = props.setView;
+    this.totalPrice = props.totalPrice;
   }
 
   render() {
-    return null;
+    return (
+      <div className='card my-4'>
+        <div className='card-body d-flex flex-column justify-content-around'>
+          <div style={{ cursor: 'pointer' }} onClick={this.setView}>
+            <i className="fas fa-chevron-left mr-2"/>
+            <h6 className='d-inline'>Back to Catalog</h6>
+          </div>
+          <h2 className="border-bottom my-2 pb-2">Your Total: ${this.totalPrice}</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="customerName">Name</label>
+              <input type="text" id='customerName'/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="cardNumber">Credit Card</label>
+              <input type="text" id='customerName'/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="customerAddress">Name</label>
+              <input type="textarea" id='customerAddress'/>
+            </div>
+            <button type="submit">Place Order</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
